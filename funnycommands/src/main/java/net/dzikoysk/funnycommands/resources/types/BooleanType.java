095a5f26528352c5c3ed7db3c9298de76d8080da
crejk <bcrejk@protonmail.com>
2020-04-19 16:45:57 +0200

    GH-15 Support basic types mapping by default  (#19)
    * gh-15 Support basic types mapping by default (Resolve #15)
    
    * remove finals
    
    * Pass default parameters through the constructor

---
+++ funnycommands/src/main/java/net/dzikoysk/funnycommands/resources/types/BooleanType.java
@@ -0,0 +1,56 @@
+package net.dzikoysk.funnycommands.resources.types;
+
+import net.dzikoysk.funnycommands.resources.CommandDataType;
+import net.dzikoysk.funnycommands.resources.Origin;
+import net.dzikoysk.funnycommands.stereotypes.FunnyComponent;
+
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
+@FunnyComponent
+public final class BooleanType implements CommandDataType<Boolean> {
+
+    private static final List<String> DEFAULT_TRUE_VALUES = Arrays.asList("true", "yes", "prawda", "tak", "y", "1");
+    private static final List<String> DEFAULT_FALSE_VALUES = Arrays.asList("false", "no", "falsz", "nie", "n", "0");
+
+    private final List<String> trueValues;
+    private final List<String> falseValues;
+
+    public BooleanType(List<String> trueValues, List<String> falseValues) {
+        this.trueValues = trueValues;
+        this.falseValues = falseValues;
+    }
+
+    public BooleanType() {
+        this(DEFAULT_TRUE_VALUES, DEFAULT_FALSE_VALUES);
+    }
+
+    @Override
+    public Boolean apply(Origin origin, Parameter parameter, String argument) {
+        if (containsIgnoreCase(trueValues, argument)) {
+            return true;
+        }
+
+        if (containsIgnoreCase(falseValues, argument)) {
+            return false;
+        }
+
+        return null;
+    }
+
+    private boolean containsIgnoreCase(List<String> values, String argument) {
+        for (String value : values) {
+            if (value.equalsIgnoreCase(argument)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return "boolean";
+    }
+
+}
---
+++ funnycommands/src/main/java/net/dzikoysk/funnycommands/resources/types/LongType.java
@@ -0,0 +1,23 @@
+package net.dzikoysk.funnycommands.resources.types;
+
+import net.dzikoysk.funnycommands.FunnyCommandsUtils;
+import net.dzikoysk.funnycommands.resources.CommandDataType;
+import net.dzikoysk.funnycommands.resources.Origin;
+import net.dzikoysk.funnycommands.stereotypes.FunnyComponent;
+
+import java.lang.reflect.Parameter;
+
+@FunnyComponent
+public final class LongType implements CommandDataType<Long> {
+
+    @Override
+    public Long apply(Origin origin, Parameter parameter, String argument) {
+        return FunnyCommandsUtils.parseNumber(argument, Long::parseLong);
+    }
+
+    @Override
+    public String getName() {
+        return "long";
+    }
+
+}
