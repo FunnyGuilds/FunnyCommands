0bc4a82d71e1e3e9ab42eca57eb285c9ce95035a
crejk <crejk@ctemplar.com>
2020-06-21 14:57:20 +0200

    GH-38 Empty completer (Resolve #38)

---
+++ funnycommands/src/main/java/net/dzikoysk/funnycommands/resources/completers/EmptyCompleter.java
@@ -0,0 +1,23 @@
+package net.dzikoysk.funnycommands.resources.completers;
+
+import net.dzikoysk.funnycommands.resources.Completer;
+import net.dzikoysk.funnycommands.resources.Origin;
+import net.dzikoysk.funnycommands.stereotypes.FunnyComponent;
+
+import java.util.Collections;
+import java.util.List;
+
+@FunnyComponent
+public final class EmptyCompleter implements Completer {
+
+    @Override
+    public List<String> apply(Origin origin, String prefix, Integer limit) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+
+}
