b777501b755d351163e2f0e4041dc00c07bd26dc
crejk <crejk@ctemplar.com>
2020-06-21 02:41:46 +0200

    GH-36 Release 0.2.1 (Resolve #36)

--- funnycommands/src/main/java/net/dzikoysk/funnycommands/FunnyCommandsConstants.java
+++ funnycommands/src/main/java/net/dzikoysk/funnycommands/FunnyCommandsConstants.java
@@ -18,6 +18,6 @@ package net.dzikoysk.funnycommands;
 
 public final class FunnyCommandsConstants {
 
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
 
 }
